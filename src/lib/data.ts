992ec1018eb660d931ed264ae0b887f09f742af4
Micheal Parks <michealparks1989@gmail.com>
2024-03-18 16:46:44 -0400

    update resume

--- src/lib/data.ts
+++ src/lib/data.ts
@@ -1,8 +1,8 @@
 export const header = {
-	name: 'Micheal Parks',
-	title: 'Software Engineer',
-	portrait: 'me.jpg',
-	description: `
+  name: "Micheal Parks",
+  title: "Software Engineer",
+  portrait: "me.jpg",
+  description: `
 		I have an ongoing love affair with science and artistic communication, yet I’m pragmatic and an engineer at heart.
 		I love the craft of storytelling, which makes me sensitive to user experience and fuels my passion for video game development.
 		I’m fascinated by the web and it’s transformative power for society.
@@ -11,134 +11,151 @@ export const header = {
 		so it doesn’t take me long to pick up a new concept after getting my hands dirty.
 		I like the mantra that one should make their work “as simple as possible, but no simpler”,
 		which makes programming for me a continual joy and challenge.
-	`
-}
+	`,
+};
 
 export const contact = {
-	email: 'michealparks1989@gmail.com',
-	phone: '1-718-362-7680',
-	location: 'Brooklyn, NY',
-	website: 'parks.lol',
-	github: 'github.com/michealparks',
-	linkedin: 'linkedin.com/in/michealparks'
-}
+  email: "michealparks1989@gmail.com",
+  phone: "1-718-362-7680",
+  location: "Brooklyn, NY",
+  website: "parks.lol",
+  github: "github.com/michealparks",
+  linkedin: "linkedin.com/in/michealparks",
+};
 
 export const jobs = [
-	{
-		title: 'Senior Sofware Engineer II - Frontend',
-		company: 'VIAM',
-		duration: 'April 2022 - Current',
-		description: `
+  {
+    title: "Senior Sofware Engineer II - Frontend",
+    company: "VIAM",
+    duration: "April 2022 - Current",
+    description: `
 			At Viam I served as a founding engineer for <a class="link" target="_tab" href="https://app.viam.com">app.viam.com</a>.
 			I designed and led development on much of the app's client architecture, deployment and CI workflows, and <a class="link" target="_tab" href="https://prime.viam.com">component library</a>.
 			I established a UI guild and taught a Three.js course to a team of engineers. I often collaborated with the design team for creating the company's design language,
 			rules for interaction, and the UI / UX of specific features such as Viam's robot designer. I prototyped and led design and development of multiple features,
 			including visualizers for viewing point clouds and for configuring the reference frames and geometry of a robot when motion planning.
 		`,
-	}, {
-		title: 'Senior Sofware Engineer - Frontend',
-		company: 'Bowery Farming',
-		duration: 'June 2021 - April 2022',
-		description: `
+  },
+  {
+    title: "Senior Sofware Engineer - Frontend",
+    company: "Bowery Farming",
+    duration: "June 2021 - April 2022",
+    description: `
 			I established code standards, design patterns, and helped grow a Frontend engineering culture for BoweryOS: the app that orchestrates and monitors
 			Bowery's farms. I mentored engineers and set up tools and practices that increased the overall reliability and positive developer experience when
 			building upon and maintaining the app.
 		`,
-	}, {
-		title: 'Lead Software Engineer',
-		company: 'MongoDB',
-		duration: 'May 2015 – Nov 2020',
-		description: `
+  },
+  {
+    title: "Lead Software Engineer",
+    company: "MongoDB",
+    duration: "May 2015 – Nov 2020",
+    description: `
 			I led a team that worked on mongodb.com, an internal MongoDB-backed CMS, and engineering projects within the larger marketing team.
 			We regularly collaborated with other engineering teams on projects such as <a class="link" target="_tab" href="https://www.mongodb.com/atlas/data-lake-demo">interactive web apps</a> that demoed MongoDB cloud products.
 			I onboarded, mentored, and collaborated with multiple front-end and full-stack developers on my team.
-		`
-	}, {
-		title: 'Lead Frontend Developer',
-		company: 'Pixbi',
-		duration: 'Nov 2013 – Apr 2015',
-		description: `
+		`,
+  },
+  {
+    title: "Lead Frontend Developer",
+    company: "Pixbi",
+    duration: "Nov 2013 – Apr 2015",
+    description: `
 			Pixbi enabled users to view, share, and purchase products from images in blogs and digital magazines.
 			I led the development of the embedded javascript applications that powered Pixbi.
-		`
-	}, {
-		title: 'New Media Web Developer',
-		company: 'University of Central Florida',
-		duration: '2012 - 2013',
-		description: `
+		`,
+  },
+  {
+    title: "New Media Web Developer",
+    company: "University of Central Florida",
+    duration: "2012 - 2013",
+    description: `
 			I wrote educational javascript games and embeddable widgets - like <a class="link" target="_tab" href="https://materia.ucf.edu/widgets/12-hangman">Hangman</a> and <a class="link" target="_tab" href="https://materia.ucf.edu/widgets/9-flash-cards">Flashcards</a> - alongside Lego models and some fantastic people.
-		`
-	}
-]
+		`,
+  },
+];
 
 export const education = [
-	{
-		institution: 'Brooklyn College',
-		duration: '2017 - 2020',
-		concentration: 'Mathematics courses'
-	}, {
-		institution: 'University of Central Florida',
-		duration: '2008 - 2013',
-		concentration: 'Biology, Computer Science and Psychology Minor'
-	}
-]
+  {
+    institution: "Brooklyn College",
+    duration: "2017 - 2020",
+    concentration: "Mathematics courses",
+  },
+  {
+    institution: "University of Central Florida",
+    duration: "2008 - 2013",
+    concentration: "Biology, Computer Science and Psychology Minor",
+  },
+];
 
 export const projects = [
-	{
-		title: 'Odyssey',
-		link: 'https://michealparks.github.io/odyssey/',
-		description: `
+  {
+    title: "Threlte",
+    link: "https://threlte.xyz",
+    description: `
+      I became a core Threlte maintainer after creating the <a class="link" target="_tab" href="">@threlte/xr</a> package.
+      Since then I've made a number of core contributions, strengthening the project's position as a powerful Three.js framework.
+    `,
+  },
+  {
+    title: "Odyssey",
+    link: "https://michealparks.github.io/odyssey/",
+    description: `
 			Odyssey was made for 2023 Svelte Hack. It is a minigame - built with Svelte and Three.js - about being lost in space.
-		`
-	}, {
-		title: 'TsunamiWorm',
-		link: 'https://michealparks.github.io/game-off-2021/',
-		description: `
+		`,
+  },
+  {
+    title: "TsunamiWorm",
+    link: "https://michealparks.github.io/game-off-2021/",
+    description: `
 			TsunamiWorm was an entry for the 2021 Game Off competition. It was made using React Three Fiber and XState. In it you play as a computer virus
 			attempting to enslave and control the resources of a poor, helpless computer.
-		`
-	}, {
-		title: 'Galeri',
-		link: 'https://galeri.io',
-		description: `
-			Galeri is a desktop app that changes your desktop background or browser's new tab page into 
+		`,
+  },
+  {
+    title: "Galeri",
+    link: "https://galeri.io",
+    description: `
+			Galeri is a desktop app that changes your desktop background or browser's new tab page into
 			a stream of artworks selected from open art APIs provided by museums and other institutions. It was recently re-written in Rust using Tauri.
-		`
-	}, {
-		title: 'Renewal',
-		link: 'https://github.com/michealparks/global-game-jam-2021',
-		description: `
+		`,
+  },
+  {
+    title: "Renewal",
+    link: "https://github.com/michealparks/global-game-jam-2021",
+    description: `
 			Renewal is a small 3d simulation game made for the 2021 global game jam using ThreeJS.
 			The gameplay is focused on turning an abandoned lot into a community garden.
-		`
-	}, {
-		title: 'House Party',
-		link: 'https://github.com/michealparks/global-game-jam-19',
-		description: `
+		`,
+  },
+  {
+    title: "House Party",
+    link: "https://github.com/michealparks/global-game-jam-19",
+    description: `
 			House Party is a small 2d pixel art game made for the 2018 global game jam featuring a minimal self-made WebGL rendering engine.
 			You play as an enraged dad who - upon finding that your kids threw a house party without your knowledge - vanguishes troublesome teens
 			with your parental powers.
-		`
-	},
-]
+		`,
+  },
+];
 
 export const skills = [
-	{ name: 'Untyped Typescript', mastery: 100000 },
-	{ name: 'CSS, HTML, The Web™', mastery: 95000 },
-	{ name: 'ThreeJS, Game Engines', mastery: 90000 },
-	{ name: 'Blender (donut making)', mastery: 85000 },
-	{ name: 'Rust', mastery: -100 },
-]
+  { name: "Untyped Typescript", mastery: 100000 },
+  { name: "CSS, HTML, The Web™", mastery: 95000 },
+  { name: "ThreeJS, Game Engines", mastery: 90000 },
+  { name: "Blender (donut making)", mastery: 85000 },
+  { name: "Rust", mastery: -100 },
+];
 
 export const interests = [
-	'3D Graphics',
-	'VR + AR',
-	'UI Engineering',
-	'UX Design',
-	'Web Apps',
-	'Interaction Design',
-	'Game Development',
-	'People-first work culture',
-	'Science',
-	'Art',
-]
+  "3D Graphics",
+  "VR + AR",
+  "UI Engineering",
+  "UX Design",
+  "Web Apps",
+  "Interaction Design",
+  "Game Development",
+  "People-first work culture",
+  "Science",
+  "Art",
+];

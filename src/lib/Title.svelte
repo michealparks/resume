2df74f9bf618be3b154d7d2d0f50ee6fc78b81f3
Micheal Parks <michealparks1989@gmail.com>
2024-05-31 14:08:19 -0400

    upgrade deps

--- src/lib/Title.svelte
+++ src/lib/Title.svelte
@@ -1,5 +1,11 @@
+<script lang='ts'>
+  import type { Snippet } from "svelte";
+
+	let { children }: { children: Snippet } = $props()
+</script>
+
 <h2 class='my-2.5'>
-	<slot />
+	{@render children()}
 </h2>
 
-<div class='h-1 w-full mb-2.5 bg-divider' />
+<div class='h-1 w-full mb-2.5 bg-divider'></div>

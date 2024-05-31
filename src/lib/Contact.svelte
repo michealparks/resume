6cb088ddcb0d1177f23b0b0b98e7a3d7fd5ba39c
Micheal Parks <michealparks1989@gmail.com>
2024-05-31 14:11:11 -0400

    lint

--- src/lib/Contact.svelte
+++ src/lib/Contact.svelte
@@ -1,74 +1,75 @@
-<script lang='ts'>
-	import Title from './Title.svelte'
-	import { contact } from './data'
+<script lang="ts">
+  import Title from "./Title.svelte";
+  import { contact } from "./data";
 </script>
 
 <section>
-	<Title>Contact</Title>
-	<div>
-		<span class='icon-envelope' />
-		<a href={`mailto:${contact.email}`}>{contact.email}</a>
-	</div>
+  <Title>Contact</Title>
+  <div>
+    <span class="icon-envelope"></span>
+    <a href={`mailto:${contact.email}`}>{contact.email}</a>
+  </div>
 
-	<div>
-		<span class='icon-phone' />
-		<a href={`tel:${contact.phone}`}>{contact.phone}</a>
-	</div>
+  <div>
+    <span class="icon-phone"></span>
+    <a href={`tel:${contact.phone}`}>{contact.phone}</a>
+  </div>
 
-	<div>
-		<span class='icon-location' />
-		<p class='m-0'>{contact.location}</p>
-	</div>
+  <div>
+    <span class="icon-location"></span>
+    <p class="m-0">{contact.location}</p>
+  </div>
 
-	<div>
-		<span class='icon-website' />
-		<a target='_tab' href={`https://${contact.website}`}>{contact.website}</a>
-	</div>
-	
-	<div>
-		<span class='icon-github' />
-		<a target='_tab' href={`https://${contact.github}`}>{contact.github}</a>
-	</div>
+  <div>
+    <span class="icon-website"></span>
+    <a target="_tab" href={`https://${contact.website}`}>{contact.website}</a>
+  </div>
 
-	<div>
-		<span class='icon-linkedin' />
-		<a target='_tab' href={`https://${contact.linkedin}`}>{contact.linkedin}</a>
-	</div>
+  <div>
+    <span class="icon-github"></span>
+    <a target="_tab" href={`https://${contact.github}`}>{contact.github}</a>
+  </div>
+
+  <div>
+    <span class="icon-linkedin"></span>
+    <a target="_tab" href={`https://${contact.linkedin}`}>{contact.linkedin}</a>
+  </div>
 </section>
 
 <style>
-	section {
-		grid-row: 1 / auto;
-		grid-column: 1 / span 1;
-	}
+  section {
+    grid-row: 1 / auto;
+    grid-column: 1 / span 1;
+  }
 
-	div {
-		display: flex;
-		padding: 8px 0;
-	}
+  div {
+    display: flex;
+    padding: 8px 0;
+  }
 
-	@media (min-width: 700px) {
-		div {
-			padding: 4px 0;
-		}
-	}
+  @media (min-width: 700px) {
+    div {
+      padding: 4px 0;
+    }
+  }
 
-	span {
-		margin-right: 5px;
-		display: flex;
-		align-items: center;
-	}
+  span {
+    margin-right: 5px;
+    display: flex;
+    align-items: center;
+  }
 
-	a, p {
-		font-size: 13px;
-	}
+  a,
+  p {
+    font-size: 13px;
+  }
 
-	a {
-		color: inherit;
-		text-decoration: none;
-	}
+  a {
+    color: inherit;
+    text-decoration: none;
+  }
 
-	a:hover {
-		text-decoration: underline;
-	}
+  a:hover {
+    text-decoration: underline;
+  }
 </style>
--- src/lib/Skills.svelte
+++ src/lib/Skills.svelte
@@ -1,30 +1,32 @@
-<script lang='ts'>
-	import Title from './Title.svelte'
-	import { skills } from './data'
+<script lang="ts">
+  import Title from "./Title.svelte";
+  import { skills } from "./data";
 </script>
 
 <section>
-	<Title>Skillz</Title>
-	<ul class='text-xs'>
-		{#each skills as { name, mastery } (name)}
-			<li class='
+  <Title>Skillz</Title>
+  <ul class="text-xs">
+    {#each skills as { name, mastery } (name)}
+      <li
+        class="
 				relative flex justify-between items-center overflow-hidden
 				my-1.5 py-1.5 px-1.5 rounded border border-accent dark:border-slate-600 dark:text-gray-300
-			'>
-				<div
-					class='absolute top-0 left-0 h-full -z-[1] progress bg-accent dark:bg-slate-600'
-					style='width: {(mastery / 100000) * 100}%'
-				/>
-				{name}
-				<small>{mastery.toLocaleString('en-US')}%</small>
-			</li>
-		{/each}
-	</ul>
+			"
+      >
+        <div
+          class="absolute top-0 left-0 h-full -z-[1] progress bg-accent dark:bg-slate-600"
+          style="width: {(mastery / 100000) * 100}%"
+        ></div>
+        {name}
+        <small>{mastery.toLocaleString("en-US")}%</small>
+      </li>
+    {/each}
+  </ul>
 </section>
 
 <style>
-	section {
-		grid-row: 4 / auto;
-		grid-column: 1 / span 1;
-	}
+  section {
+    grid-row: 4 / auto;
+    grid-column: 1 / span 1;
+  }
 </style>
